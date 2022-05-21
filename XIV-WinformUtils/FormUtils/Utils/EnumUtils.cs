d16ea3823784519434b6ba934e8d9a462cac4fad
alimertcetin <mrtmert54@gmail.com>
2022-05-22 02:47:10 +0300

    Methods for index based operations

--- XIV-WinformUtils/FormUtils/Utils/EnumUtils.cs
+++ XIV-WinformUtils/FormUtils/Utils/EnumUtils.cs
@@ -7,17 +7,35 @@ namespace XIV.Utils
     /// </summary>
     public static class EnumUtils
     {
-        public static string GetTypeString<T>(object value) where T : Enum
+        //TODO : Add summary to methods
+
+        public static string GetName<T>(object value) where T : Enum
         {
             return Enum.GetName(typeof(T), value);
         }
 
-        public static T GetType<T>(string value) where T : Enum
+        public static int GetIndex<T>(object value) where T : Enum
+        {
+            var type = typeof(T);
+            Array values = Enum.GetValues(type);
+            int counter = 0;
+            foreach (var item in values)
+            {
+                if (item == value)
+                {
+                    return counter;
+                }
+                counter++;
+            }
+            return -1;
+        }
+
+        public static T GetValueByName<T>(string valueName) where T : Enum
         {
             Array values = Enum.GetValues(typeof(T));
             foreach (var item in values)
             {
-                if (item.ToString() == value)
+                if (item.ToString() == valueName)
                 {
                     return (T)item;
                 }
@@ -25,9 +43,38 @@ namespace XIV.Utils
             return default(T);
         }
 
+        public static T GetValueByIndex<T>(int index) where T : Enum
+        {
+            Array values = Enum.GetValues(typeof(T));
+            int counter = 0;
+            foreach (var item in values)
+            {
+                if (counter == index)
+                {
+                    return (T)item;
+                }
+                counter++;
+            }
+            return default(T);
+        }
+
         public static Array GetValues<T>() where T : Enum
         {
             return Enum.GetValues(typeof(T));
+        }
+
+        public static string GetNameByIndex<T>(int index) where T : Enum
+        {
+            var value = GetValueByIndex<T>(index);
+            var name = GetName<T>(value);
+            return name;
+        }
+
+        public static int GetIndexByName<T>(string valueName) where T : Enum
+        {
+            var val = GetValueByName<T>(valueName);
+            var index = GetIndex<T>(val);
+            return index;
         }
     }
 }
